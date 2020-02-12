002c20aa175471e444dc5f3bc9c67cedf369f8ae
Michael Bryan <michaelbryan.eng@gmail.com>
2020-02-12 13:56:13 -0300

    first commit

---
+++ ppmtool-java-api/src/main/java/com/bryan/ppmtool/PpmtoolJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.bryan.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtoolJavaApiApplication.class, args);
+	}
+
+}
