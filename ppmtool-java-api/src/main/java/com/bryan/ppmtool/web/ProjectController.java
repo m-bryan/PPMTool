da4b85d966c68b3b6c549eed969e53fadbedafda
Michael Bryan <michaelbryan.eng@gmail.com>
2020-02-12 16:59:11 -0300

    Project controller refactor

--- ppmtool-java-api/src/main/java/com/bryan/ppmtool/web/ProjectController.java
+++ ppmtool-java-api/src/main/java/com/bryan/ppmtool/web/ProjectController.java
@@ -1,22 +1,18 @@
 package com.bryan.ppmtool.web;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bryan.ppmtool.domain.Project;
+import com.bryan.ppmtool.services.MapValidationErrorService;
 import com.bryan.ppmtool.services.ProjectService;
 
 @RestController
@@ -26,13 +22,14 @@ public class ProjectController {
 	@Autowired
 	private ProjectService projectService;
 	
+	@Autowired
+	private MapValidationErrorService mapValidationErrorService;
+	
 	@PostMapping("")
 	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
 		
-		Map<String, String> mapError = new HashMap<String, String>();
-		for(FieldError error: result.getFieldErrors()) mapError.put(error.getField(), error.getDefaultMessage());
-		
-		if(result.hasErrors()) return new ResponseEntity<Map<String, String>>(mapError, HttpStatus.BAD_REQUEST);
+		ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationErrorService(result);
+		if(errorMap != null) return errorMap;
 		
 		Project project1 = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
