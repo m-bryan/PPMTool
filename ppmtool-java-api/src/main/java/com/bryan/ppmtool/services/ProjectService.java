ff73619d594a980e33b5d49abcc600dc14d9ce62
Michael Bryan <michaelbryan.eng@gmail.com>
2020-02-14 15:38:37 -0300

    Fid project by identifier

--- ppmtool-java-api/src/main/java/com/bryan/ppmtool/services/ProjectService.java
+++ ppmtool-java-api/src/main/java/com/bryan/ppmtool/services/ProjectService.java
@@ -12,15 +12,25 @@ public class ProjectService {
 
 	@Autowired
 	private ProjectRepository projectRepository;
-	
+
 	public Project saveOrUpdateProject(Project project) {
 		try {
 			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
 			return projectRepository.save(project);
 		} catch (Exception e) {
-			throw new ProjectIdException("Prject ID " + project.getProjectIdentifier().toUpperCase() + " already exists");
+			throw new ProjectIdException(
+					"Prject ID " + project.getProjectIdentifier().toUpperCase() + " already exists");
 		}
-		
+
 	}
-	
+
+	public Project findProjectByProjectId(String projectIdentifier) {
+		Project project = projectRepository.findByProjectIdentifier(projectIdentifier.toUpperCase());
+		if (project == null) {
+			throw new ProjectIdException("Prject ID " + projectIdentifier.toUpperCase() + " does not exists");
+		}
+
+		return project;
+	}
+
 }
