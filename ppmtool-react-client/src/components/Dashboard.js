f31fc3c29cd5d272260c459a4125b559d496f62b
Bryan <michaelbryan.eng@gmail.com>
2020-02-15 00:50:22 -0300

    First component

---
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import ProjectItem from './project/ProjectItem'
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <h1>Dashboard Component</h1>
+            
+        )
+    }
+}
+
+export default Dashboard;
